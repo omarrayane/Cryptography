b339bf2885216558ead44e129d47b71dadaf78de
YaniAnis <yanilikou02@gmail.com>
2026-05-02 19:53:57 +0100

    Update HMAC.py

--- Hashing/HMAC.py
+++ Hashing/HMAC.py
@@ -5,14 +5,13 @@ def hmac_hash():
     word = input("Enter the word to hash with HMAC: ")
     key = input("Enter the secret key for HMAC: ")
     
-    # Create an HMAC object using the secret key, the word, and a hash function (e.g., SHA256)
+   
     hmac_obj = hmac.new(
         key.encode('utf-8'),
         word.encode('utf-8'),
         hashlib.sha256
     )
-    
-    # Get the hexadecimal representation of the HMAC
+
     hashed_word = hmac_obj.hexdigest()
     print(f"HMAC (SHA256) Hash: {hashed_word}")
 
